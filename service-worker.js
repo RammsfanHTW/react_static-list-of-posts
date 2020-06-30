899b42b4d81b633ee37b9fbeffeef9b25acd6a91
RammsfanHTW <64034634+RammsfanHTW@users.noreply.github.com>
2020-06-30 20:07:43 +0300

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "./precache-manifest.1b6ea50562cf8abf248f73f3d229fa90.js"
+  "./precache-manifest.3a308ae6163b79bcb6cb7e6a6f3f9292.js"
 );
 
 self.addEventListener('message', (event) => {
